c95f3603c9197fd2d23515cf3acc4c8b5cbf2e47
IsakGairatov <isak3141592653@gmail.com>
2023-04-17 16:49:19 +0600

    Created Models interfaces

---
+++ Lab10/Project/hh-front/src/app/Models/Company.ts
@@ -0,0 +1,28 @@
+export interface Company {
+    id: Number;
+    name: String;
+    description: String;
+    city: String;
+    address: String;
+}
+
+export const COMPANY: Company[] = [
+  {
+    id: 1,
+    name: "Fairy tailes",
+    description : "JFJFssgdg",
+    city: "34 jja",
+    address: "fopjpejpfojsze"},
+  {
+    id: 3,
+    name: "bkbskbnkdfs",
+    description : "JFJFssgdg",
+    city: "34 jja",
+    address: "fopjpejpfojsze"},
+  {
+    id: 4,
+    name: "grdhrhhdrh",
+    description : "JFJFssgdg",
+    city: "34 jja",
+    address: "fopjpejpfojsze"}
+]
---
+++ Lab10/Project/hh-front/src/app/Models/Vacancy.ts
@@ -0,0 +1,31 @@
+export interface Vacancy{
+  id: Number;
+  name: String;
+  description: String;
+  salary: Number;
+  company: Number;
+}
+
+export const Vacs: Vacancy[] = [
+  {
+    id: 4,
+    name: "Fairy Tail Worker 1",
+    description: "Nonename",
+    salary: 4563.0,
+    company: 1,
+  },
+  {
+    id: 7,
+    name: "Faker 1",
+    description: "Nonename",
+    salary: 4563.0,
+    company: 1,
+  },
+  {
+    id: 6,
+    name: "ryker 14",
+    description: "Nonename",
+    salary: 4563.0,
+    company: 1,
+  }
+]
